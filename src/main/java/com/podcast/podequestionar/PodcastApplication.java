0e38b679444b4c9726004650a5938fb8cccf7d7e
Felipendev <felipe.psic@outlook.com.br>
2022-04-27 08:40:50 -0300

    Primeiro commit

---
+++ src/main/java/com/podcast/podequestionar/PodcastApplication.java
@@ -0,0 +1,14 @@
+package com.podcast.podequestionar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastApplication.class, args);
+		System.out.println("Estou funcionando");
+	}
+
+}
---
+++ src/test/java/com/podcast/podequestionar/PodcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podcast.podequestionar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
